150888bce18e2e28d623227d25ecc08aab5763b1
Jens Becker <info@jensbecker.dev>
2021-06-20 15:05:06 +0200

    docs: document adaptive helper classes

--- lib/src/adaptive_helpers.dart
+++ lib/src/adaptive_helpers.dart
@@ -1,21 +1,43 @@
 import 'package:universal_platform/universal_platform.dart';
 
+/// Class whose attributes help to detect the current device type.
+///
+/// This uses the awesome [universal_platform](https://pub.dev/packages/universal_platform) package.
 class DeviceType {
-  // Syntax sugar, proxy the UniversalPlatform methods so our views can reference a single API
+  /// Whether the app is running on iOS.
   static bool isIOS = UniversalPlatform.isIOS;
+
+  /// Whether the app is running on android.
   static bool isAndroid = UniversalPlatform.isAndroid;
+
+  /// Whether the app is running on macOS.
   static bool isMacOS = UniversalPlatform.isMacOS;
+
+  /// Whether the app is running on linux.
   static bool isLinux = UniversalPlatform.isLinux;
+
+  /// Whether the app is running on windows.
   static bool isWindows = UniversalPlatform.isWindows;
 
-  // Higher level device class abstractions (more syntax sugar for the views)
+  /// Whether the app is running on the web.
   static bool isWeb = UniversalPlatform.isWeb;
+
+  /// Whether the app is running on desktop (windows, macOS or linux).
   static bool isDesktop = isWindows || isMacOS || isLinux;
+
+  /// Whether the app is running on mobile (android or iOS).
   static bool isMobile = isAndroid || isIOS;
+
+  /// Whether the app is running on desktop or web.
   static bool isDesktopOrWeb = isDesktop || isWeb;
+
+  /// Whether the app is running on mobile or web.
   static bool isMobileOrWeb = isMobile || isWeb;
 }
 
+/// A single source of truth for styling values like padding, spacing, corner shape, font sizes, and so on.
+///
+/// These constants can then be used in place of hard-coded numeric values.
 class Insets {
   /// An extra extra small inset of 2.0.
   static const double xxs = 2.0;
